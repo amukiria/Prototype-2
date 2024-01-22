9828710e88d32c4fda29270a6d3a274010db8fc0
Albert Mukiria <albert.mukiria@gmail.com>
2024-01-22 12:25:58 +0300

    Completed Challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnCooldown = 2f; // Set the cooldown time in seconds
+    private float lastSpawnTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if enough time has passed since the last spawn
+        if (Time.time - lastSpawnTime >= spawnCooldown)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastSpawnTime = Time.time; // Update the last spawn time
+            }
+        }
+    }
+}
